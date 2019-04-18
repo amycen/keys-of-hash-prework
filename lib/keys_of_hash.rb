64a88110e5ac5abebe4b3f8c3757790bded8fc33
Amy Cen <amy.cen@blackrock.com>
2019-04-18 02:48:47 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,10 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    keys = []
+    arguments.each do |val|
+      keys << Hash.key(val)
+    end
+    keys
   end
 end
